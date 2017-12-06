1212f7b8e2e83cdbe3d445942483b9040b51029f
Tommaso Soru <mommi84@gmail.com>
2017-12-06 11:44:35 +0100

    spaces as neural SPARQL token separators

--- build_vocab.py
+++ build_vocab.py
@@ -11,29 +11,39 @@ with open(sys.argv[1]) as f:
     for line in f:
         x_text.append(unicode(line[:-1]))
 
-# x_text = ['This is a cat','This must be boy', 'This is a a dog']
-max_document_length = max([len(x.split(" ")) for x in x_text])
+vocabulary = set()
 
-## Create the vocabularyprocessor object, setting the max lengh of the documents.
-vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
+if sys.argv[1][-3:] == ".en":
 
-## Transform the documents using the vocabulary.
-x = np.array(list(vocab_processor.fit_transform(x_text)))    
+    # x_text = ['This is a cat','This must be boy', 'This is a a dog']
+    max_document_length = max([len(x.split(" ")) for x in x_text])
 
-## Extract word:id mapping from the object.
-vocab_dict = vocab_processor.vocabulary_._mapping
+    ## Create the vocabularyprocessor object, setting the max lengh of the documents.
+    vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
 
-## Sort the vocabulary dictionary on the basis of values(id).
-## Both statements perform same task.
-#sorted_vocab = sorted(vocab_dict.items(), key=operator.itemgetter(1))
-sorted_vocab = sorted(vocab_dict.items(), key = lambda x : x[1])
+    ## Transform the documents using the vocabulary.
+    x = np.array(list(vocab_processor.fit_transform(x_text)))    
 
-## Treat the id's as index into list and create a list of words in the ascending order of id's
-## word with id i goes at index i of the list.
-vocabulary = list(list(zip(*sorted_vocab))[0])
+    ## Extract word:id mapping from the object.
+    vocab_dict = vocab_processor.vocabulary_._mapping
 
+    ## Sort the vocabulary dictionary on the basis of values(id).
+    ## Both statements perform same task.
+    #sorted_vocab = sorted(vocab_dict.items(), key=operator.itemgetter(1))
+    sorted_vocab = sorted(vocab_dict.items(), key = lambda x : x[1])
+
+    ## Treat the id's as index into list and create a list of words in the ascending order of id's
+    ## word with id i goes at index i of the list.
+    vocabulary = set(list(zip(*sorted_vocab))[0])
+
+if sys.argv[1][-7:] == ".sparql":
+    
+    for x in x_text:
+        for t in x.split(" "):
+            vocabulary.add(t)
+    
 # print(vocabulary)
 # print(x)
 for v in vocabulary:
-    print v
-
+    if v != "":
+        print v
